123d002a00c04341b1824e2ab558474f8c0c51c6
Remo <r.basteck@gmail.com>
2024-12-27 09:02:21 +0100

    chore: review feedback

--- RpgStats.BlazorServer/Model/RpgStatsResponse.cs
+++ RpgStats.BlazorServer/Model/RpgStatsResponse.cs
@@ -3,6 +3,6 @@ namespace RpgStats.BlazorServer.Model;
 public class RpgStatsResponse<T>
 {
     public bool Success { get; set; }
-    public string? ErrorMessage { get; set; }
+    public string ErrorMessage { get; set; } = "An error occurred";
     public T? Data { get; set; }
 }
