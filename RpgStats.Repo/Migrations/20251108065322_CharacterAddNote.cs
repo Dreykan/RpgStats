19285214675bae39706ec6ffd65e5b4c114ce151
Remo <r.basteck@gmail.com>
2025-11-08 10:12:58 +0100

    chore: push migration for adding note at character

---
+++ RpgStats.Repo/Migrations/20251108065322_CharacterAddNote.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RpgStats.Repo.Migrations
+{
+    /// <inheritdoc />
+    public partial class CharacterAddNote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                table: "Characters",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Note",
+                table: "Characters");
+        }
+    }
+}
